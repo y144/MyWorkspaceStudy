85b412290cd238a28a0f65161f165d81b1d6b25b
yang <yanglun@126.com>
2022-12-28 13:48:00 +0800

    中缀表达式转后缀表达式并计算出结果

--- MyStudy/DataStructureAndAlgorithms/src/main/java/com/yangtignjia/stack/PolandNotation.java
+++ MyStudy/DataStructureAndAlgorithms/src/main/java/com/yangtignjia/stack/PolandNotation.java
@@ -16,11 +16,17 @@ public class PolandNotation {
         //说明
         //1. 1 + ((2 + 3) * 4) - 5 => 1 2 3 + 4 * + 5 -
         //2.因为不方便直接对字符串进行扫描  则将中缀表达式的字符串放入list中
-        //3.中缀表达式转后缀表达式
-        String expression = "1+((2+3)*4)-5";
-        List<String> list = toInfixExpression(expression);
-        System.out.println(list);
+        //3.中缀表达式对应的list转为对应的后缀表达式list
 
+        String expression = "1+((2+3)*4)-5";
+        System.out.println("中缀表达式 = " + expression);
+        List<String> list = toInfixExpression(expression);
+        System.out.println("中缀表达式对应的list = " + list);
+
+        List<String> parseSuffixExpressionList = parseSuffixExpressionList(list);
+        System.out.println("后缀表达式对应的list = " + parseSuffixExpressionList);
+
+        System.out.printf("expression = %d\n" , calculate(parseSuffixExpressionList));
 
 
         //先定义一个逆波兰表达式 (3+4) * 5 -6 --> 3 4 + 5 * 6 -
@@ -38,7 +44,44 @@ public class PolandNotation {
 
     // 将中缀表达式的list 转为 对应的后缀表达式的list
     public static List<String> parseSuffixExpressionList(List<String> list) {
-        return list;
+        //定义两个栈
+        Stack<String> s1 = new Stack<String>(); // 符号栈
+        //应为s2这个栈，在整个转换过程中没有pop操作，而且最后我们还要逆序输出结果
+        //因此比较麻烦，因此我们在这里不使用栈了就直接使用一个list
+        //Stack<String> s2 = new Stack<String>(); //中间结果栈
+        List<String> s2 = new ArrayList<String>();
+
+        //遍历list
+        for (String item : list) {
+            //如果是一个数，就加入数栈 s2
+            if (item.matches("\\d+")) {
+                s2.add(item);
+            }else if (item.equals("(")) {
+                s1.push(item);
+            }else if (item.equals(")")) {
+                //如果是右括号 ） 就一次弹出s1栈顶的运算符，并压入s2，直到遇到左括号为止，此时将这一对括号丢弃
+                while (!s1.peek().equals("(")) {
+                    s2.add(s1.pop());
+                }
+                s1.pop(); //将 左括号 （ 弹出s1栈，消除小括号
+            }else {
+                //当item运算符的优先级小于或等于s1栈顶运算符，将s1栈顶的运算符弹出并加入到s2中，
+                // 再次转到4.1中与s1中新的栈顶运算符相比较
+                // 问题：缺少一个比较优先级高低的方法
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) >= Operation.getValue(item)) {
+                    s2.add(s1.pop());
+                }
+                //还需要将item压入符号栈
+                s1.push(item);
+            }
+        }
+
+        //将s1中剩余的运算符依次压入s2
+        while (s1.size() != 0) {
+            s2.add(s1.pop());
+        }
+        //注意 因为是将结果存放到list中，因此按照list的顺序输出就是对应的后缀表达式对应的list
+        return s2;
     }
 
     //将中缀表达式转换为对应的list
@@ -115,4 +158,39 @@ public class PolandNotation {
         //最后留在stack中的数就是结果
         return Integer.parseInt(stack.pop());
     }
+}
+
+//这个类返回一个运算符对应的优先级
+class Operation {
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
+
+    //方法 返回对应优先级的数字
+    public static int getValue(String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = ADD;
+                break;
+            case "-":
+                result = SUB;
+                break;
+            case "*":
+                result = MUL;
+                break;
+            case "/":
+                result = DIV;
+                break;
+            case "(":
+                break;
+            case ")":
+                break;
+            default:
+                throw new RuntimeException("不存在该运算符");
+        }
+        return result;
+    }
+
 }
